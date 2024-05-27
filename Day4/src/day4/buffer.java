244c5e7e4c206821c30601df45b3cc4e1e06668c
Keerthanaporchezhiyan <mcafkeerthanaporchezhiyan@gmail.com>
2024-05-27 08:52:18 +0530

    Add files via upload

---
+++ Day4/src/day4/buffer.java
@@ -0,0 +1,16 @@
+package day4;
+import java.io.*;
+
+public class buffer {
+	String ename;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	System.out.println("Enter your name");
+	String name = br.readLine();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
